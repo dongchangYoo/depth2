59ce36b044c827597e7b1f15fa7aa8efeadfc83a
dongchangYoo <dongchang.yoo@pi-lab.io>
2022-12-04 22:22:52 +0900

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="depth2",
-    version="v0.0.1",
+    version="0.0.2",
     package=find_packages,
     install_requires=[],
     dependency_links=[""]
